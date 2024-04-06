5b45b3ba0b76b971f5aa08ec5fa9ee1f772e6be7
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-06 12:51:50 +0200

    created mailable and message blade

---
+++ resources/views/mails/contacts/message.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boolfolio</title>
+    <style>
+        body {
+            background-color: lightblue;
+            text-align: center;
+            font-family: 'Courier New', Courier, monospace;
+        }
+    </style>
+</head>
+
+<body>
+    <p>É stato inviato un messaggio</p>
+
+</body>
+
+</html>
